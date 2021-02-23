56f5ddd79217143d96683cb6072272fb36b50c10
SeungHyun <fm10033@gmail.com>
2021-02-24 02:24:38 +0900

    db 넣는 페이지 완성, db 구축 완료, 검색 기능 오류수정중

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta
+db = client.DBFINDA
 
 @app.route('/')
 def home():
@@ -23,5 +23,44 @@ def return_result():
 def retrun_search():
     return render_template('search.html')
 
+@app.route('/getdb')
+def return_getdb():
+    return render_template('scraping.html')
+
+
+@app.route('/getdb',methods=['POST'])
+def post_db():
+   poster_receive = request.form['poster_give']
+   title_receive = request.form['title_give']
+   year_receive = request.form['year_give']
+   director_receive = request.form['director_give']
+   genre_receive = request.form.getlist('genre_give[]')
+   actor_receive = request.form.getlist('actor_give[]')
+   platformname_receive = request.form.getlist('platformName_give[]')
+   iconurl_receive = request.form['iconUrl_give']
+   desc_receive = request.form['desc_give']
+
+   doc = {
+       'poster': poster_receive,
+       'title': title_receive,
+       'year': year_receive,
+       'director': director_receive,
+       'genre': genre_receive,
+       'actor': actor_receive,
+       'platformname': platformname_receive,
+       'iconurl': iconurl_receive,
+       'desc': desc_receive
+   }
+   db.movie.insert_one(doc)
+
+   return jsonify({'result': 'success', 'msg': 'db에 잘 들어감!'})
+
+@app.route('/main', methods=['POST'])
+def get_search_data():
+    search_receive = request.form['search_give']
+    search_list = list(db.movie.find(
+        {'$or':[ {'title': {'$regex': search_receive}}]},{'_id': False}
+    ))
+    return jsonify({'result': 'success', 'data': search_list})
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000, debug=True)
