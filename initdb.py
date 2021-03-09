e405c0a483b37d1c9317e505163bd4f0171c1299
SeungHyun <fm10033@gmail.com>
2021-03-09 18:54:32 +0900

    og 태그 작성

--- initdb.py
+++ initdb.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-aws_client = MongoClient('mongodb://test:test@192.32.19.21', 27017)
+aws_client = MongoClient('mongodb://test:test@54.180.112.223', 27017)
 aws_db = aws_client.DBFINDA
 local_client = MongoClient('localhost', 27017)
 local_db = local_client.DBFINDA
